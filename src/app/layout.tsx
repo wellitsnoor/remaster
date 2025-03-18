1911996bad513e01a20934d60a150115bdffd37c
wellitsnoor PC <famousfortech500@gmail.com>
2025-03-18 19:15:48 +0530

    home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
 import "./globals.css";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
 
 export const metadata: Metadata = {
   title: "Remaster",
@@ -19,7 +13,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+      <link rel="icon" href="/logo.jpg" />
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+"use client";
+
 import React from "react";
-import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function page() {
   return (
-    <div className="flex m-auto justify-center items-center h-screen">
-      <p className="text-5xl font-bold text-remaster">REMASTER</p>
+    <div className="w-fit h-fit fixed top-0 bottom-0 right-0 left-0 m-auto text-center justify-center items-center flex remaster text-remaster">
+      {["R", "E", "M", "A", "S", "T", "E", "R"].map((letter, index) => (
+        <motion.div
+          key={index}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            ease: "easeInOut",
+            duration: 1,
+            delay: (index + 1) * 0.1,
+            repeat: Infinity,
+            repeatType: "mirror",
+            repeatDelay: 0,
+          }}
+          className="text-5xl font-bold text-red-700"
+        >
+          {letter}
+        </motion.div>
+      ))}
     </div>
   );
 }
